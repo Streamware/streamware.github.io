60cf613a9b24f1ec6f7a92bf055af0532a2ef508
Hasan Haghniya <secretkeeper@streamware.co>
2024-03-22 18:56:14 +0330

    declared types for publishedAt

--- app/publishedAt.tsx
+++ app/publishedAt.tsx
@@ -1,7 +1,11 @@
 "use client";
 import TimeAgo from "react-timeago";
 
-export default function PublishedAt({ date }) {
+interface PublishedAtProps {
+	date: string;
+}
+
+export default function PublishedAt({ date }: PublishedAtProps) {
 	return (
 		<p className="text-[10px] text-gray-700">
 			Published <TimeAgo date={date} />
